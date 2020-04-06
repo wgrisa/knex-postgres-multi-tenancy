435164727feef75045b491f079149ea653a13092
william <grisa.william@gmail.com>
2020-04-06 12:28:03 -0300

    Update scripts to use docker-compose v3

--- spec/fixtures/knexfile.js
+++ spec/fixtures/knexfile.js
@@ -1,24 +1,13 @@
-import url from 'url'
-
-const client = process.env.DB_CLIENT
-
-function getEnv(type) {
-  const name = client.toUpperCase()
-  return process.env[`${name}_${type}`] || process.env[`${name}_ENV_${name}_${type}`]
-}
-
-const address = url.parse(process.env.DB_PORT || getEnv('PORT'))
-
 export default {
   test: {
-    client: client === 'postgres' ? 'pg' : client,
+    client: 'pg',
     connection: {
       multipleStatements: true,
-      host: address.hostname,
-      port: address.port,
-      user: process.env.DB_USER || getEnv('USER'),
-      password: process.env.DB_PASSWORD || getEnv('PASSWORD'),
-      database: process.env.DB_DATABASE || getEnv('DB') || getEnv('DATABASE'),
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
     },
     migrations: {
       directory: './spec/fixtures/db/migrations',
